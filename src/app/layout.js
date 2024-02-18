6f391956862ae5b15b14d79724d1b781bb330762
unknown <jitender>
2024-02-18 18:38:50 +0530

    ssl issue resolve

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning >
-    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"></meta>
       <body className={inter.className}>{children}</body>
     </html>
   );
